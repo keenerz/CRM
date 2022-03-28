b897ee1d3e40c88c89c4aa6f425cf3063df1bba8
Keenan <kkan68@gmail.com>
2022-03-28 23:51:54 +0000

    added some flux need to modify

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,59 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      todos: [],
+    },
+    actions: {
+      //Login and Token items
+      getCurrentSession: () => {
+        const session = JSON.parse(localStorage.getItem("session"));
+        return session;
+      },
+      login: async (email, password) => {
+        const store = getStore();
+        const actions = getActions();
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+        try {
+          const response = await fetch(
+            process.env.BACKEND_URL + `/api/token`,
+            options
+          );
+          if (response.status !== 200) {
+            alert("Incorrect Email or Password");
+            return false;
+          }
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+          const data = await response.json();
+          localStorage.setItem("session", JSON.stringify(data));
+          setStore({ session: data });
+          actions.loadProjects();
+          actions.getUser();
+          return true;
+        } catch (error) {
+          console.error("Error in login zone");
+        }
+      },
+      logout: () => {
+        const store = getStore();
+        const actions = getActions();
+        localStorage.removeItem("session");
+        setStore({ session: null });
+        localStorage.removeItem("useredit");
+        setStore({ useredit: null });
+        actions.loadProjects();
+      },
+    },
+  };
 };
 
 export default getState;
