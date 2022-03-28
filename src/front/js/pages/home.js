93874fa3c8bf708b3efa3d62ac0b0fa057bc8c9f
Keenan <kkan68@gmail.com>
2022-03-28 15:19:02 +0000

    added task list components

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,22 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState } from "react";
+import TaskList from "../component/tasklist";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+const Home = () => {
+  return (
+    <div className="container-flex">
+      <div className="pt-5 text-center mx-auto">
+        <h1 className="fw-light title opacity-25">todos</h1>
+      </div>
+      <div className="row px-5 d-flex justify-content-center" id="outerTask">
+        <div
+          className="text-center d-flex justify-content-center"
+          id="innerTask"
+        >
+          <TaskList />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Home;
